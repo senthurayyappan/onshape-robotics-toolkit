50a2d5649d6d4e95d0f56b5abd51888d90997760
imsenthur <imsenthur@gmail.com>
2024-10-22 08:18:59 -0400

    Minor fix to mass property.

--- examples/bike/main.py
+++ examples/bike/main.py
@@ -1,7 +1,7 @@
 import onshape_api as osa
 
+# Initialize the client with the constructed path
 client = osa.Client()
-
 doc = osa.Document.from_url(
     url="https://cad.onshape.com/documents/a1c1addf75444f54b504f25c/w/0d17b8ebb2a4c76be9fff3c7/e/a86aaf34d2f4353288df8812"
 )
@@ -16,8 +16,10 @@ variables["forkAngle"].expression = "30 deg"
 client.set_variables(doc.did, doc.wid, elements["variables"].id, variables)
 assembly = client.get_assembly(doc.did, doc.wtype, doc.wid, elements["assembly"].id)
 
-for i, part in enumerate(assembly.parts):
+# for i, part in enumerate(assembly.parts):
     # client.download_stl(part.documentId, doc.wid, part.elementId, part.partId, save_path=f"./{part.partId}_{i}.stl")
-    print(client.get_mass_properties(part.documentId, doc.wid, part.elementId, part.partId))
+    # print(client.get_mass_properties(part.documentId, doc.wid, part.elementId, part.partId))
 
 # print(client.get_features_from_assembly(doc.did, doc.wtype, doc.wid, elements["assembly"].id))
+
+print(assembly)
